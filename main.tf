10bab3c0da67468cd0502f6287550ba67866689e
GANESH KALNE <66562863+kalneganesh@users.noreply.github.com>
2023-01-02 17:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #Add AWS as porvider
 provider "aws" {
   region     = "ap-south-2"
-  access_key = "AKIAYXDGW7FLIZWT4WU4"
-  secret_key = "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"
+  access_key = "AKIAYXDGW7FLIZWT4WU410"
+  secret_key = "S3j6AkBKrrfyRlzckS1nJcdK/qGvB+J9ZZIWd77/g"
 }
